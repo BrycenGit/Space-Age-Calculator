4a6899d181fe6fe93217acfa23a1f88ca66066cb
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-04 15:11:24 -0700

    add asset folder

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,25 @@ module: {
         ],
       loader: "eslint-loader"
     }
+    {
+      test: /\.(gif|png|jpe?g)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/images/'
+          }
+        }
+      ]
+    },
+    
+    {
+      test:/\.html$/,
+      use: [
+        'html-loader'
+      ]
+    },
   ]
 }
 };
